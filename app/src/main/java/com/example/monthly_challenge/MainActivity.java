796ff218522a448c55bd9b8cbe4df0a0b08d027e
홍승현 <hsh@hongseunghyeon-ui-MacBookAir.local>
2020-08-15 00:12:58 +0900

    start

--- app/src/main/java/com/example/monthly_challenge/MainActivity.java
+++ app/src/main/java/com/example/monthly_challenge/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.monthly_challenge;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.monthly_challenge.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setActivity(this);
     }
 }
