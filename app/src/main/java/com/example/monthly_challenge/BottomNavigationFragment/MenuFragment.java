1404f8f445ab130d300537b40202aa6164f6fcc2
홍승현 <hsh@hongseunghyeon-ui-MacBookAir.local>
2020-08-18 18:49:10 +0900

    drawer navi 수정

--- app/src/main/java/com/example/monthly_challenge/BottomNavigationFragment/MenuFragment.java
+++ app/src/main/java/com/example/monthly_challenge/BottomNavigationFragment/MenuFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,11 +14,17 @@ import com.example.monthly_challenge.R;
 
 public class MenuFragment extends Fragment {
     ViewGroup viewGroup;
+    Button change1;
+    Fragment fragment;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_menu, container, false);
+
         return viewGroup;
     }
+
+
 }
