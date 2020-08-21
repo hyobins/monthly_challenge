b81985b493eb9ac82af027d606316f2ed84cb7a3
홍승현 <hsh@hongseunghyeon-ui-MacBookAir.local>
2020-08-21 18:43:37 +0900

    프로젝트 타이틀, 마감일, 상금 연동

--- app/src/main/java/com/example/monthly_challenge/BottomNavigationFragment/MenuFragment.java
+++ app/src/main/java/com/example/monthly_challenge/BottomNavigationFragment/MenuFragment.java
@@ -2,11 +2,9 @@ package com.example.monthly_challenge.BottomNavigationFragment;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -14,87 +12,142 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import com.example.monthly_challenge.ListItem;
+import com.example.monthly_challenge.MainActivity;
 import com.example.monthly_challenge.R;
-import com.example.monthly_challenge.databinding.ActivityMainBinding;
-import com.example.monthly_challenge.databinding.FragmentMenuBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MenuFragment extends Fragment implements View.OnClickListener {
     ViewGroup viewGroup;
-    ScrollView projectInfoView;
-    ScrollView projectIngView;
-    LinearLayout card0;
-    LinearLayout infoLayout;
-    LinearLayout submitLayout;
-    LinearLayout[] card;
-    View infoView;
-    View submitView;
-    TextView infoText;
-    TextView submitText;
+    @BindView(R.id.projectInfoView) ScrollView projectInfoView;
+    @BindView(R.id.projectIngView) ScrollView projectIngView;
+    @BindView(R.id.infoLayout) LinearLayout infoLayout;
+    @BindView(R.id.submitLayout) LinearLayout submitLayout;
+    @BindView(R.id.linearLayout_list0) LinearLayout linearLayout_list0;
+    @BindView(R.id.linearLayout_list1) LinearLayout linearLayout_list1;
+    @BindView(R.id.linearLayout_list2) LinearLayout linearLayout_list2;
+    @BindView(R.id.linearLayout_list3) LinearLayout linearLayout_list3;
+    @BindView(R.id.linearLayout_list4) LinearLayout linearLayout_list4;
+    @BindView(R.id.linearLayout_list5) LinearLayout linearLayout_list5;
+    LinearLayout[] linearLayout_lists;
+    @BindView(R.id.textView_listTitle0) TextView textView_listTitle0;
+    @BindView(R.id.textView_listTitle1) TextView textView_listTitle1;
+    @BindView(R.id.textView_listTitle2) TextView textView_listTitle2;
+    @BindView(R.id.textView_listTitle3) TextView textView_listTitle3;
+    @BindView(R.id.textView_listTitle4) TextView textView_listTitle4;
+    @BindView(R.id.textView_listTitle5) TextView textView_listTitle5;
+    TextView[] textView_listTitles;
+    @BindView(R.id.textView_listDeadline0) TextView textView_listDeadline0;
+    @BindView(R.id.textView_listDeadline1) TextView textView_listDeadline1;
+    @BindView(R.id.textView_listDeadline2) TextView textView_listDeadline2;
+    @BindView(R.id.textView_listDeadline3) TextView textView_listDeadline3;
+    @BindView(R.id.textView_listDeadline4) TextView textView_listDeadline4;
+    @BindView(R.id.textView_listDeadline5) TextView textView_listDeadline5;
+    TextView[] textView_listDeadlines;
+    @BindView(R.id.textView_listReward0) TextView textView_listReward0;
+    @BindView(R.id.textView_listReward1) TextView textView_listReward1;
+    @BindView(R.id.textView_listReward2) TextView textView_listReward2;
+    @BindView(R.id.textView_listReward3) TextView textView_listReward3;
+    @BindView(R.id.textView_listReward4) TextView textView_listReward4;
+    @BindView(R.id.textView_listReward5) TextView textView_listReward5;
+    TextView[] textView_listRewards;
+
+    @BindView(R.id.infoView) View infoView;
+    @BindView(R.id.submitView) View submitView;
+    @BindView(R.id.infoText) TextView infoText;
+    @BindView(R.id.submitText) TextView submitText;
 
     LinearLayout prevLayout;
     View prevView;
     TextView prevText;
 
-    ImageView backButton;
+    @BindView(R.id.backButton) ImageView backButton;
 
-    TextView title0;
-    TextView titleText;
+    @BindView(R.id.titleText) TextView titleText;
+    @BindView(R.id.deadlineText) TextView deadlineText;
+    @BindView(R.id.rewardText) TextView rewardText;
+
+    ArrayList<ListItem> listItems;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_menu, container, false);
+        ButterKnife.bind(this, viewGroup);
         projectInfoView = viewGroup.findViewById(R.id.projectInfoView);
         projectIngView = viewGroup.findViewById(R.id.projectIngView);
-        card0 = viewGroup.findViewById(R.id.card0);
-//        card = new LinearLayout[]{card1};
 
-        infoLayout = viewGroup.findViewById(R.id.infoLayout);
-        infoView = viewGroup.findViewById(R.id.infoView);
-        infoText = viewGroup.findViewById(R.id.infoText);
-        submitLayout = viewGroup.findViewById(R.id.submitLayout);
-        submitView = viewGroup.findViewById(R.id.submitView);
-        submitText = viewGroup.findViewById(R.id.submitText);
 
-        backButton = viewGroup.findViewById(R.id.backButton);
+        linearLayout_lists = new LinearLayout[]{linearLayout_list0,linearLayout_list1,linearLayout_list3,
+                                                linearLayout_list4,linearLayout_list5};
 
-        title0 = viewGroup.findViewById(R.id.textView21);
-        titleText = viewGroup.findViewById(R.id.titleText);
+        textView_listTitles = new TextView[]{textView_listTitle0,textView_listTitle1,textView_listTitle2,
+                                            textView_listTitle3,textView_listTitle4,textView_listTitle5};
+        textView_listDeadlines = new TextView[]{textView_listDeadline0,textView_listDeadline1,
+                                    textView_listDeadline3,textView_listDeadline4,textView_listDeadline5};
+        textView_listRewards = new TextView[]{textView_listReward0,textView_listReward1,textView_listReward2,
+                                    textView_listReward3,textView_listReward4,textView_listReward5};
+
 
         prevLayout = infoLayout;
         prevView = infoView;
         prevText = infoText;
 
-        card0.setOnClickListener(this);
+        listItems = MainActivity.getListItem();
+        System.out.println(listItems);
+
+        for(int i=0;i<linearLayout_lists.length;i++){
+            linearLayout_lists[i].setOnClickListener(this);
+        }
         infoText.setOnClickListener(this);
         submitText.setOnClickListener(this);
         backButton.setOnClickListener(this);
 
-        db.collection("project")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                title0.setText(document.get("title").toString());
-//                                Log.d(TAG, document.getId() + " => " + document.getData());
-                            }
-                        } else {
-//                            Log.w(TAG, "Error getting documents.", task.getException());
-                        }
-                    }
-                });
+        if(listItems.size() <= linearLayout_lists.length){
+            for(int i=0;i<listItems.size();i++){
+                textView_listTitles[i].setText(listItems.get(i).getTitle());
+                textView_listDeadlines[i].setText(listItems.get(i).getDeadline());
+                textView_listRewards[i].setText(listItems.get(i).getReward());
+            }
+        }
+        else{
+            for(int i=0;i<linearLayout_lists.length;i++){
+                textView_listTitles[i].setText(listItems.get(i).getTitle());
+                textView_listDeadlines[i].setText(listItems.get(i).getDeadline());
+                textView_listRewards[i].setText(listItems.get(i).getReward());
+            }
+        }
+
+//        db.collection("project")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                ListItem listItem = new ListItem(document.get("title").toString(),document.get("deadline").toString(),document.get("reward").toString());
+////                                Log.d(TAG, document.getId() + " => " + document.getData());
+//                            }
+//                        } else {
+////                            Log.w(TAG, "Error getting documents.", task.getException());
+//                        }
+//                    }
+//                });
 
         return viewGroup;
     }
@@ -116,11 +169,37 @@ public class MenuFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+        TextView textView;
         switch (v.getId()){
-            case R.id.card0:
-                projectIngView.setVisibility(View.GONE);
-                projectInfoView.setVisibility(View.VISIBLE);
-                titleText.setText(title0.getText().toString());
+            case R.id.linearLayout_list0:
+                textView = textView_listTitle0;
+                if(textView.getText().equals("")) break;
+                onClickListItem(0);
+                break;
+            case R.id.linearLayout_list1:
+                textView = textView_listTitle1;
+                if(textView.getText().equals("")) break;
+                onClickListItem(1);
+                break;
+            case R.id.linearLayout_list2:
+                textView = textView_listTitle2;
+                if(textView.getText().equals("")) break;
+                onClickListItem(2);
+                break;
+            case R.id.linearLayout_list3:
+                textView = textView_listTitle3;
+                if(textView.getText().equals("")) break;
+                onClickListItem(3);
+                break;
+            case R.id.linearLayout_list4:
+                textView = textView_listTitle4;
+                if(textView.getText().equals("")) break;
+                onClickListItem(4);
+                break;
+            case R.id.linearLayout_list5:
+                textView = textView_listTitle5;
+                if(textView.getText().equals("")) break;
+                onClickListItem(5);
                 break;
             case R.id.submitText:
 
@@ -140,4 +219,12 @@ public class MenuFragment extends Fragment implements View.OnClickListener {
 
         }
     }
+    private void onClickListItem(int i){
+        projectIngView.setVisibility(View.GONE);
+        projectInfoView.setVisibility(View.VISIBLE);
+        titleText.setText(textView_listTitles[i].getText().toString());
+        deadlineText.setText(textView_listDeadlines[i].getText().toString());
+        rewardText.setText(textView_listRewards[i].getText().toString());
+    }
+
 }
