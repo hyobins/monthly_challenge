9b6863aa282fa1913df77f166033e871c209c0a9
홍승현 <hsh@hongseunghyeon-ui-MacBookAir.local>
2020-08-22 16:02:29 +0900

    document id 추가, 팀모집 탭 활성화

--- app/src/main/java/Project/EndListItem.java
+++ app/src/main/java/Project/EndListItem.java
@@ -1,13 +1,19 @@
-package com.example.monthly_challenge;
+package Project;
 
-public class JudgeListItem {
+public class EndListItem {
+    private String projectId;
     private String title;
     private String deadline;
     private String reward;
-    public JudgeListItem(String title, String deadline, String reward) {
+    public EndListItem(String projectId, String title, String deadline, String reward) {
+        this.projectId = projectId;
         this.title = title;
         this.deadline = deadline;
         this.reward = reward;
+    }
+
+    public String getProjectId() {
+        return this.projectId;
     }
 
     public String getTitle() {
